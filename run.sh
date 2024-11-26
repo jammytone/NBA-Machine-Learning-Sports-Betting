bcd30f536c88e92f8178d4c5bed74f8d671061cf
Choi-jaehoon <zxc131@naver.com>
2024-11-27 06:26:10 +0900

    Clean up repository and add .gitignore

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# 예측 실행 및 대시보드 업데이트
-python main.py -xgb -odds fanduel -kc 
--- src/Utils/OddsConverter.py
+++
@@ -1,8 +0,0 @@
-def american_to_decimal(american_odds):
-    """아메리칸 배당률을 소수점 배당률로 변환"""
-    if not american_odds:
-        return None
-    if american_odds > 0:
-        return round(1 + (american_odds / 100), 2)
-    else:
-        return round(1 + (100 / abs(american_odds)), 2)
